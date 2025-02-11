f4222f1ffdc14a0956d997f1e372ac23055225c3
gagoto-dev <gagotodev@gmail.com>
2025-02-11 09:40:34 +0100

    autocmds file + telescope cmd fixed

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -6,4 +6,4 @@ vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 
 end)
-vim.keymap.set('n', '<leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
--- lua/gagoto/init.lua
+++ lua/gagoto/init.lua
@@ -1,3 +1,4 @@
 require("gagoto.plugins")
 require("gagoto.remap")
 require("gagoto.set")
+require("gagoto.autocmds")
