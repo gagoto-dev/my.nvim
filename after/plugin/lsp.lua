8c07e9284f08899a403ba9aa91c5821f95dbb9d0
gagoto-dev <gagotodev@gmail.com>
2025-02-17 22:56:06 +0100

    null ls removed, lsp conform config changed

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,5 +1,20 @@
 require("conform").setup({
-	formatters_by_ft = {},
+	formatters_by_ft = {
+		lua = { "stylua" },
+		go = { "goimports", "gofmt" },
+		php = { "phpactor" },
+	},
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
+	-- Set the log level. Use `:ConformInfo` to see the location of the log file.
+	log_level = vim.log.levels.ERROR,
+	-- Conform will notify you when a formatter errors
+	notify_on_error = true,
+	-- Conform will notify you when no formatters are available for the buffer
+	notify_no_formatters = true,
 })
 local cmp = require("cmp")
 local cmp_lsp = require("cmp_nvim_lsp")
