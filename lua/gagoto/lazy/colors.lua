68cce73d2d9dd5cec9e14cd89cc4104043c47e7e
Gaspar Gómez <gagotodev@gmail.com>
2025-03-25 19:09:34 +0100

    changes

--- lua/gagoto/lazy/colors.lua
+++ lua/gagoto/lazy/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-    color = color or "onedark"
+    color = color or "tokyonight-moon"
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -28,6 +28,22 @@ return {
         priority = 1000,
         opts = {},
         config = function()
+            require("tokyonight").setup({
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                style = "moon",         -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+                transparent = true,     -- Enable this to disable setting the background color
+                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+                styles = {
+                    -- Style to be applied to different syntax groups
+                    -- Value is any valid attr-list value for `:help nvim_set_hl`
+                    comments = { italic = false },
+                    keywords = { italic = false },
+                    -- Background styles. Can be "dark", "transparent" or "normal"
+                    sidebars = "dark", -- style for sidebars, see below
+                    floats = "dark",   -- style for floating windows
+                },
+            })
             ColorMyPencils("tokyonight-moon")
         end,
     }
--- lua/gagoto/lazy/init.lua
+++ lua/gagoto/lazy/init.lua
@@ -1,31 +1,40 @@
 return {
-	{
-		"nvim-lua/plenary.nvim",
-		name = "plenary",
-	},
-	"eandrju/cellular-automaton.nvim",
-	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
-		"lewis6991/gitsigns.nvim",
-		opts = {
-			signs = {
-				add = { text = "+" },
-				change = { text = "~" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-			},
-		},
-	},
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		opts = {},
-	},
-	"numToStr/Comment.nvim",
-	"sbdchd/neoformat",
-	"ThePrimeagen/vim-be-good",
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-	},
+    {
+        "nvim-lua/plenary.nvim",
+        name = "plenary",
+    },
+    "eandrju/cellular-automaton.nvim",
+    { -- Adds git related signs to the gutter, as well as utilities for managing changes
+        "lewis6991/gitsigns.nvim",
+        opts = {
+            signs = {
+                add = { text = "+" },
+                change = { text = "~" },
+                delete = { text = "_" },
+                topdelete = { text = "‾" },
+                changedelete = { text = "~" },
+            },
+        },
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        opts = {},
+    },
+    "numToStr/Comment.nvim",
+    "sbdchd/neoformat",
+    "ThePrimeagen/vim-be-good",
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
+    {
+        "ThePrimeagen/vim-apm",
+        config = function()
+            local apm = require("vim-apm")
+
+            apm:setup({})
+            vim.keymap.set("n", "<leader>apm", function() apm:toggle_monitor() end)
+        end
+    }
 }
