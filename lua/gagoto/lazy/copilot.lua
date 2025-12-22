f4a599e52511fa719f90976c44d0dff96823af6d
Gaspar Gómez <gagotodev@gmail.com>
2025-12-22 18:52:29 +0100

    add: github copilot

--- lua/gagoto/lazy/copilot.lua
+++ lua/gagoto/lazy/copilot.lua
@@ -1,10 +1,10 @@
 return {
-	{
-		"github/copilot.vim",
-		name = "copilot",
-		config = function()
-			vim.g.copilot_no_tab_map = true
-			vim.api.nvim_set_keymap("i", "<C-f>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-		end,
-	},
+	-- 	{
+	-- 		"github/copilot.vim",
+	-- 		name = "copilot",
+	-- 		config = function()
+	-- 			vim.g.copilot_no_tab_map = true
+	-- 			vim.api.nvim_set_keymap("i", "<C-f>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+	-- 		end,
+	-- 	},
 }
